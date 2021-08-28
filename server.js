eca96f2f34b07f8f7fa8940077c5838e401fa1b6
MinhHa Pham <minhha@MinhHas-MacBook-Air.local>
2021-08-27 21:14:15 -0700

    added get/* route

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-//   });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
 
 app.get('/api/notes', (req, res) => {
     if (notes) {
