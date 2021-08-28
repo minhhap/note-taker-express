f5dc2cbd6db11ea5170ce21fbbe7eccd50d6572f
MinhHa Pham <minhha@MinhHas-MacBook-Air.local>
2021-08-27 21:13:11 -0700

    fixed get route

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const path = require('path');
 const { notes } = require('./db/db');
 const express = require('express');
+const { ok } = require('assert');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -25,22 +26,26 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+//   });
+
 app.get('/api/notes', (req, res) => {
-    res.json(results);
+    if (notes) {
+        res.json(notes);
+      } else {
+        res.send(404);
+      }
   });
-  
+
 app.post('/api/notes', (req, res) => {
 // set id based on what the next index of the array will be
-req.body.id = notes.length.toString();
+    // req.body.id = notes.length.toString();
+    const newNote = req.body;
+    res.json(newNote);
+    notes.push(newNote);
+});
 
-// if any data in req.body is incorrect, send 400 error back
-if (!validateNotes(req.body)) {
-    res.status(400).send('The note is not properly formatted.');
-} else {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-}
-});  
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
